780a9efc375fcf291c2ea17b99fa5b1b232d2e59
Ed Ropple <ed@edropple.com>
2017-09-18 03:20:56 -0400

    seed commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import autobind from 'auto-bind';
+
+import {
+  Button
+} from 'reactstrap';
+
+import {
+  Toolbox,
+  towercgConnect
+} from '@towercg/dashboard';
+
+
+export class ExamplePlugin extends React.Component {
+  constructor(props) {
+    super(props);
+    autobind(this);
+  }
+
+  async _ping(ev) {
+    ev && ev.preventDefault();
+
+    const {towercg} = this.props;
+    const ret = await towercg.invoke("example.ping");
+
+    console.log(ret);
+  }
+
+  render() {
+    return (
+      <Toolbox.DashboardPluginBody
+        key="example"
+        title="Example Plugin"
+        xs={12} sm={12} md={6}
+      >
+        <p>
+          Ticks: <strong>{this.props.ticks}</strong>
+        </p>
+        <Button onClick={this._ping}>Ping to Console</Button>
+      </Toolbox.DashboardPluginBody>
+    );
+  }
+}
+
+const wrapped = towercgConnect(ExamplePlugin, (state) => ({
+  ticks: state.example.ticks
+}));
+export default wrapped;
